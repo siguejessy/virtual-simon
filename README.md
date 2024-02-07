e3dfb131e74c1a6e0f20f292760722391eb2244e
siguejessy <jessicamendez328@gmail.com>
2024-02-07 10:14:08 -0500

    html & css elements in place

--- README.md
+++ README.md
@@ -1,13 +1,61 @@
-# Simon
+# Virtual Simon
 ## Read Me: 
-Simon - An interactive memorization game.
+### Simon - An interactive memorization game.
+🟢 🔴<br>
+🟡 🔵<br>
 
-This is a virtual recreation of the real-world popular toy, Simon, co-invented by “The Father of Video Games”, Ralph H. Baer and released by Milton-Bradley in 1978.
+This is a virtual recreation of the real-world popular toy, Simon, co-invented by “The Father of Video Games”, Ralph H. Baer, and released by Milton-Bradley in 1978.
 
-Fun-fact: The original Simon’s grand debut was at a special Midnight Release Party during the 1978 holiday season at Studio 54 in NYC!
+Fun-fact: The original Simon’s grand debut was at it's special Midnight Release Party at Studio 54 during the 1978 holiday season in NYC! ❄️💃🏼🪩
+</br></br>
+
+### Virtual Simon
+**Objective:** Beat your previous high score (or a friend’s high score!) by correctly repeating Simon’s pattern and advancing through rounds.
 
 
-Objective: Beat your previous high score (or a friend’s high score!) by correctly repeating Simon’s pattern and advancing through rounds.
+**How to play:** Simon is a game of mimic and memorization! Simon is an expert pattern builder and will build a sequential pattern for you to mimic before advancing to the next round.
 
+To start the game, press the ‘Start’ button at the center of the Simon circle board. 
+A button will automatically light up and play a sound (that’s Simon giving you the first pattern to mimic!).
+Click on the dimmed buttons in the same sequence that Simon showed for each round to advance to the next round.
 
-How to play Simon: Simon is a game of mimic and memorization! Simon is an expert pattern builder and will build a sequential pattern for you to mimic before advancing to the next round.
+Simon will add another button to the sequence with each round you advance through.
+
+Example:
+Round 1: blue
+Round 2: blue, red
+Round 3: blue, red, blue
+Round 4: blue, red, blue, yellow
+Round 5: blue, red, blue, yellow, green
+And so on…
+
+If you don't correctly mimic Simon’s given pattern, you’ll be given the option to reset and play again.
+</br></br>
+
+**Technologies used:** 
+HTML, CSS, JavaScript, Figma
+
+**Icebox:** 
+Theme selection - mix & match between button color themes and sound themes.
+Pattern Setter: Choose between a 1 player or 2 player game.
+1 player game - Simon is the pattern setter
+2 player game - player 1 is the pattern setter
+</br></br>
+
+```
+/*---- pseudo code ----*/
+
+// initialize a new game
+
+// create two new empty arrays from the original buttons array at the start of the game. 
+
+// assign 1 new array to simon and 1 new array to Player 1
+
+// fill Simon's empty array with a random auto selected element from the original buttons array on simon's first turn. 
+
+// function to alternate whose turn it is
+
+// function to listen to Player 1's click and compare it to Simon's click
+
+// if Player 1 click === simon's click, advance round
+```
--- assets/app.js
+++ assets/app.js
@@ -1,68 +1,84 @@
-/*----- variables used to track the state of the game  -----*/
-let game;
-let curCombo;
-let combinationCount
+// /*----- variables used to track the state of the game  -----*/
+// let board;
+// let turn;
+// let round;
+let colorBtns = []; 
 
-/*----- constants in the game  -----*/
-const player1;
-const simon;
-const buttons = [green, red, yellow, blue];
-const startBtn;
+// /*----- constants in the game  -----*/
+// const player1;
+// const simon;
+// const startBtn;
 
 /*----- cached elements  -----*/
 //Store elements on the page that will be accessed in code more than once in variables 
 //to make code more concise, readable and performant.
-boardEl
-buttonsEl
+
+const colorBtnsEl = [...document.querySelectorAll('.light-buttons > div')];
+        console.log(colorBtnsEl);
+
+        const s
+
+
 //startBtnEl (not sure if this should be it's own element or include in buttonEl)
-countEl
+// countEl
 
-/*----- event listeners -----*/
-startBtnEl.addEventListener('click', function Initialize(){
-    //function to render a new board and restart the combination count
-});
-buttonsEl.addEventListener('click',function(litButton){
-    //function will render the button to it's non-default color &
-    //play the button's audio when clicked
-});
+// // /*----- event listeners -----*/
+// // startBtnEl.addEventListener('click', function Initialize(){
+// //     //function to render a new board and restart the combination count
+// // });
+// // buttonsEl.addEventListener('click',function(litButton){
+// //     //function will render the button to it's non-default color &
+// //     //play the button's audio when clicked
+// // });
 
-/*----- classes -----*/
-class button {
-    constructor() {};
-/* values 
-null:darkGrey
-1: green,
-2: red,
-3: yellow,
-4:blue */
-// render the buttons to the selected value
-}
+// // /*----- classes -----*/
+// // class button {
+// //     constructor() {};
+// // /* values 
+// // null:darkGrey
+// 1: green,
+// 2: red,
+// 3: yellow,
+// 4:blue */
+// // render the buttons to the selected value
+// }
 
-class SimonLightsGame {
-//advance round if player 1 click(s) === simon click(s).
-//if player 1 click(s) !== simon clicks, stop the game, announce combination count,
-//render restart button & prompt player 1 to try again
+// class SimonLightsGame {
+// //advance round if player 1 click(s) === simon click(s).
+// //if player 1 click(s) !== simon clicks, stop the game, announce combination count,
+// //render restart button & prompt player 1 to try again
 
-    constructor(boardEl, countEl) {};
-    //initialize new game
-    // reset combination count to 0
-    // set the first turn to simon
-}
+//     constructor(boardEl, countEl) {};
+//     //initialize new game
+//     // reset combination count to 0
+//     // set the first turn to simon
+// }
 
-/*----- functions -----*/
+// /*----- functions -----*/
+// Initialize();
 
-// pseudo code 
-//initialize a new game
+// function initialize() {
+//     board = [];
+//     turn = simon;
+//     render();
+// };
 
-//create a two new empty arrays from the original buttons array at the start
-//of the game. 
+// function render() {
 
-//assign 1 new array to simon and 1 new array to Player 1
+// };
 
-//Fill Simon's empty array with a random auto selected element from the original buttons array
-// on simon's first turn. 
+// // pseudo code 
+// //initialize a new game
 
-// function to alternate whose turn it is
+// //create a two new empty arrays from the original buttons array at the start
+// //of the game. 
 
-// function to listen to Player 1's click and compare it to Simon's click
-// if Player 1 click === simon's click, advance round.
+// //assign 1 new array to simon and 1 new array to Player 1
+
+// //Fill Simon's empty array with a random auto selected element from the original buttons array
+// // on simon's first turn. 
+
+// // function to alternate whose turn it is
+
+// // function to listen to Player 1's click and compare it to Simon's click
+// // if Player 1 click === simon's click, advance round.
